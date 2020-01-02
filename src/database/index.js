f825ec094f7521a3f739a6e7a4b3a92e513fe9a3
Lucas Siqueira <lucas.siqueira@skyone.solutions>
2020-01-02 16:32:35 -0200

    upload file endpoint

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,11 @@
 import Sequelize from 'sequelize';
-import User from '../app/models/User';
+
 import databaseConfig from '../config/database';
 
-const models = [User];
+import User from '../app/models/User';
+import File from '../app/models/File';
+
+const models = [User, File];
 
 class Database {
   constructor() {
@@ -11,7 +14,9 @@ class Database {
 
   init() {
     this.connection = new Sequelize(databaseConfig);
-    models.map(model => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 export default new Database();
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,24 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
+import authMiddleware from './app/middlewares/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
-import authMiddleware from './app/middlewares/auth';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
-// Rotas sem autenticação
+// Rotas sem autenticação : application/json
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
 
-// Rotas com autenticação
+// Rotas com autenticação : application/json
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+
+// Rotas para envio de arquivos : multipart/form-data
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
