3d67c781835088b6ac8444c0a14a01104a948d06
Alexsandro Oliveira <alexsandro.silva@skyone.solutions>
2020-01-03 18:48:44 -0300

    adding import

--- src/database/index.js
+++ src/database/index.js
@@ -4,6 +4,7 @@ import databaseConfig from '../config/database';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
+import Player from '../app/models/Player';
 
 const models = [User, File, Player];
 
